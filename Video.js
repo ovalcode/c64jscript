056f5157aa36a098b357eeedbb01913626f61f92
Johan Steenkamp <johan@johan>
2016-05-14 10:10:29 +0200

    modified:   Video.js

--- Video.js
+++ Video.js
@@ -4,21 +4,38 @@ function video(mycanvas, mem) {
   this.updateCanvas = function() {
     var imgData = ctx.createImageData(320, 200);
     var i, currentScreenPos;
+    var currentScreenX = 0;
+    var currentScreenY = 0;
     for (currentScreenPos = 0; currentScreenPos < 1000; currentScreenPos++) {
       var screenCode = localMem.readMem(1024 + currentScreenPos);
+      if (currentScreenX == 40) {
+        currentScreenX = 0;
+        currentScreenY++;
+      }
       var currentRow, currentCol;
       for (currentRow = 0; currentRow < 8; currentRow++) {
         var currentLine = localMem.readCharRom((screenCode << 3) + currentRow);
         for (currentCol = 0; currentCol < 8; currentCol++) {
-          Boolean s;
+          var pixelSet = (currentLine & 0x80) == 0x80;
+          var pixelPosX = (currentScreenX << 3) + currentCol;
+          var pixelPosY = (currentScreenY << 3) + currentRow;
+          var posInBuffer = (pixelPosY * 320 + pixelPosX) << 2;
+          if (pixelSet) {
+            imgData.data[posInBuffer + 0] = 0;
+            imgData.data[posInBuffer + 1] = 0;
+            imgData.data[posInBuffer + 2] = 0;
+            imgData.data[posInBuffer + 3] = 255;
+          } else {
+            imgData.data[posInBuffer + 0] = 255;
+            imgData.data[posInBuffer + 1] = 255;
+            imgData.data[posInBuffer + 2] = 255;
+            imgData.data[posInBuffer + 3] = 255;
+
+          }
+          currentLine = currentLine << 1;
         }
       }
-    }
-    for (i = 0; i < 200000; i += 4) {
-      imgData.data[i + 0] = 255;
-      imgData.data[i + 1] = 0;
-      imgData.data[i + 2] = 0;
-      imgData.data[i + 3] = 255;
+      currentScreenX++;
     }
     ctx.putImageData(imgData,0,0);
   }
