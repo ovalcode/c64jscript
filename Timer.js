39c900680af51dc0058f7fe44ca7c00998a0a9d6
Johan Steenkamp <johan@johan>
2016-06-05 12:27:39 +0200

    modified:   Timer.js

--- Timer.js
+++ Timer.js
@@ -3,6 +3,9 @@ function timer(alarmManager) {
   var isEnabled = false;
   var ticksBeforeExpiry = 0;
   myAlarmManager.addAlarm(this);
+  var timerHigh = 0;
+  var timerLow = 0;
+  var continious = false;
  
   this.getIsEnabled = function() {
     return isEnabled;
@@ -17,6 +20,37 @@ function timer(alarmManager) {
   }
 
   this.trigger = function() {
+  }
+
+  this.setTimerHigh = function(high) {
+    timerHigh = high;
+  }
+
+  this.setTimerLow = function(low) {
+    timerHigh = low;
+  }
+
+  this.getTimerHigh = function() {
+    return (ticksBeforeExpiry >> 8);
+  }
+
+  this.getTimerLow = function() {
+    return (ticksBeforeExpiry & 0xff);
+  }
+
+  this.setControlRegister = function (byteValue) {
+    if ((byteValue & (1 << 4)) != 0)
+      ticksBeforeExpiry = (timerHigh << 8) | timerLow;
+
+    continious = ((byteValue & (1 << 3)) == 0) ? true : false;
+
+    isEnabled = ((byteValue & 1) == 1) ? true : false;
+  
+  }
+
+  this.getControlRegister = function() {
+    var tempValue = 0;
+    
   }
 
 }
