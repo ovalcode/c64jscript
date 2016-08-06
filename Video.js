87b75b732547bcdcb99f90efb2445d54bc671170
Johan Steenkamp <johan@johan>
2016-08-06 18:56:08 +0200

    Add Sprite functionality 	modified:   Video.js 	modified:   index.html

--- Video.js
+++ Video.js
@@ -1,6 +1,10 @@
-function video(mycanvas, mem, cpu) {
+function video(backgroundCanvas, spriteBackgroundCanvas, foregroundCanvas, spriteforegroundCanvas, mem, cpu) {
   var localMem = mem;
-  var ctx = mycanvas.getContext("2d");
+  var ctxBackground = backgroundCanvas.getContext("2d");
+  var ctxSpriteBackground = spriteBackgroundCanvas.getContext("2d");
+  var ctxForeground = foregroundCanvas.getContext("2d");
+  var ctxSpriteForeground = spriteforegroundCanvas.getContext("2d");
+
   var mycpu = cpu;
   var cpuCycles = 0;
   var cycleInLine = 0;
@@ -8,8 +12,13 @@ function video(mycanvas, mem, cpu) {
   var charPosInMem = 0;  
   var registers = new Uint8Array(0x2e);
   var colorRAM = new Uint8Array(1000);
+  var spriteColorLine = new Uint8Array(48 * 4);
   var posInCanvas = 0;
-  var imgData = ctx.createImageData(400, 284);
+  var backgroundData = ctxBackground.createImageData(400, 284);
+  var spriteBackgroundData = ctxSpriteBackground.createImageData(400, 284);
+  var foregroundData = ctxForeground.createImageData(400, 284);
+  var spriteForegroundData = ctxSpriteForeground.createImageData(400, 284);
+
 
 
   const colors = [[0, 0, 0],
@@ -66,6 +75,18 @@ function video(mycanvas, mem, cpu) {
     return colorRAM[number];
   }
 
+  function getSpriteLineData (spriteNumber, spriteLine) {
+    var spritePointerAdd = (registers[0x18] >> 4) & 0xf;    
+    spritePointerAdd = spritePointerAdd << 10;
+    spritePointerAdd = spritePointerAdd + 0x3f8 + spriteNumber;
+    spriteBaseAdd = localMem.vicRead(spritePointerAdd) << 6;
+    posInSpriteData = spriteLine * 3 + spriteBaseAdd;
+    spriteDataByte0 = localMem.vicRead(posInSpriteData + 0);
+    spriteDataByte1 = localMem.vicRead(posInSpriteData + 1);
+    spriteDataByte2 = localMem.vicRead(posInSpriteData + 2);
+    return (spriteDataByte0 << 16) | (spriteDataByte1 << 8) | (spriteDataByte2 << 0);
+  }
+
   this.processpixels = function() {
     var numBytes = mycpu.getCycleCount() - cpuCycles;
     cpuCycles = mycpu.getCycleCount();
@@ -74,6 +95,7 @@ function video(mycanvas, mem, cpu) {
       if (isVisibleArea()) {
         if (isPixelArea() & displayEnabled()) {
           drawCharline();
+          processSprites();
         } else {
           fillBorderColor();
         }
@@ -171,15 +193,16 @@ function video(mycanvas, mem, cpu) {
     for (currentCol = 0; currentCol < 8; currentCol++) {
       var pixelSet = (currentLine & 0x80) == 0x80;
       if (pixelSet) {
-        imgData.data[posInCanvas + 0] = colors[textColor][0];
-        imgData.data[posInCanvas + 1] = colors[textColor][1];
-        imgData.data[posInCanvas + 2] = colors[textColor][2];
-        imgData.data[posInCanvas + 3] = 255;
+        foregroundData.data[posInCanvas + 0] = colors[textColor][0];
+        foregroundData.data[posInCanvas + 1] = colors[textColor][1];
+        foregroundData.data[posInCanvas + 2] = colors[textColor][2];
+        foregroundData.data[posInCanvas + 3] = 255;      
       } else {
-        imgData.data[posInCanvas + 0] = colors[backgroundColor][0];
-        imgData.data[posInCanvas + 1] = colors[backgroundColor][1];
-        imgData.data[posInCanvas + 2] = colors[backgroundColor][2];
-        imgData.data[posInCanvas + 3] = 255;
+        foregroundData.data[posInCanvas + 3] = 0;
+        backgroundData.data[posInCanvas + 0] = colors[backgroundColor][0];
+        backgroundData.data[posInCanvas + 1] = colors[backgroundColor][1];
+        backgroundData.data[posInCanvas + 2] = colors[backgroundColor][2];
+        backgroundData.data[posInCanvas + 3] = 255;
       }
       currentLine = currentLine << 1;
       posInCanvas = posInCanvas + 4;
@@ -205,19 +228,138 @@ function video(mycanvas, mem, cpu) {
     var pixPair = 0;
     for (pixPair = 0; pixPair < 4; pixPair++) {
       var colorValue = (currentLine >> 6) & 3;
-      imgData.data[posInCanvas + 0] = colors[colorArray[colorValue]][0];
-      imgData.data[posInCanvas + 1] = colors[colorArray[colorValue]][1];
-      imgData.data[posInCanvas + 2] = colors[colorArray[colorValue]][2];
-      imgData.data[posInCanvas + 3] = 255;
-      imgData.data[posInCanvas + 4] = colors[colorArray[colorValue]][0];
-      imgData.data[posInCanvas + 5] = colors[colorArray[colorValue]][1];
-      imgData.data[posInCanvas + 6] = colors[colorArray[colorValue]][2];
-      imgData.data[posInCanvas + 7] = 255;
+      if (colorArray >= 2) {
+        foregroundData.data[posInCanvas + 0] = colors[colorArray[colorValue]][0];
+        foregroundData.data[posInCanvas + 1] = colors[colorArray[colorValue]][1];
+        foregroundData.data[posInCanvas + 2] = colors[colorArray[colorValue]][2];
+        foregroundData.data[posInCanvas + 3] = 255;
+        foregroundData.data[posInCanvas + 4] = colors[colorArray[colorValue]][0];
+        foregroundData.data[posInCanvas + 5] = colors[colorArray[colorValue]][1];
+        foregroundData.data[posInCanvas + 6] = colors[colorArray[colorValue]][2];
+        foregroundData.data[posInCanvas + 7] = 255;      } else {
+        backgroundData.data[posInCanvas + 0] = colors[colorArray[colorValue]][0];
+        backgroundData.data[posInCanvas + 1] = colors[colorArray[colorValue]][1];
+        backgroundData.data[posInCanvas + 2] = colors[colorArray[colorValue]][2];
+        backgroundData.data[posInCanvas + 3] = 255;
+        foregroundData.data[posInCanvas + 3] = 0;
+        backgroundData.data[posInCanvas + 4] = colors[colorArray[colorValue]][0];
+        backgroundData.data[posInCanvas + 5] = colors[colorArray[colorValue]][1];
+        backgroundData.data[posInCanvas + 6] = colors[colorArray[colorValue]][2];
+        backgroundData.data[posInCanvas + 7] = 255;
+        foregroundData.data[posInCanvas + 7] = 0;      }
 
       currentLine = currentLine << 2;
       posInCanvas = posInCanvas + 8;
    }
   }
+
+  function processSprites() {
+    i = 0;
+    var spriteBit = 0x80;
+    var lineSegmentStart = cycleInLine << 3;
+    var lineSegmentStop = lineSegmentStart | 7;
+    for (i = 0; i < 8; i++) {
+      var currentSprite = 7 - i;
+      spriteBit = 0x80 >> i;
+      if ((registers[0x15] & spriteBit) == 0)
+        continue;
+      var xExpanded = (registers[0x1d] & spriteBit) != 0;
+      var yExpanded = (registers[0x17] & spriteBit) != 0;
+      var xDimension = xExpanded ? 48: 24;
+      var yDimension = yExpanded ? 42: 21;
+      var spritePosX = registers[currentSprite << 1];
+      if ((registers[0x10] & spriteBit) != 0) {
+        spritePosX = spritePosX | 0x100;
+      }
+      var spritePosY = registers[(currentSprite << 1) | 1];
+      if (!((cycleline >= spritePosY) & (cycleline < (spritePosY + yDimension))))
+        continue;
+      var lineScenario = 0;
+      if (((lineSegmentStart >= spritePosX) & (lineSegmentStart < (spritePosX + xDimension))))
+        lineScenario = 2;
+
+      if (((lineSegmentStop >= spritePosX) & (lineSegmentStop < (spritePosX + xDimension))))
+        lineScenario = lineScenario | 1;
+
+      if (lineScenario == 0)
+        continue;
+
+      var requiredLineInSprite = cycleline - spritePosY;
+      if (yExpanded)
+        requiredLineInSprite = requiredLineInSprite >> 1;
+      
+      var spriteIsMultiColor = (registers[0x1c] & spriteBit) != 0
+      if (yExpanded) {
+        if (spriteIsMultiColor) {
+          populateSpriteMultiColorLineExpanded(currentSprite, requiredLineInSprite);
+        } else {
+          populateSpriteColorLineExpanded(currentSprite, requiredLineInSprite);
+        }
+      } else {
+        if (spriteIsMultiColor) {
+          populateSpriteMultiColorLine(currentSprite, requiredLineInSprite);
+        } else {
+          populateSpriteColorLine(currentSprite, requiredLineInSprite);
+        }
+      }
+
+      
+      var canvasForSprite;
+      if ((registers[0x1b] & spriteBit) == 0) {
+        canvasForSprite = spriteForegroundData;
+      } else {
+        canvasForSprite = spriteBackgroundData;
+      }
+      var posInSpriteCanvas = posInCanvas - 8;
+      if (lineScenario == 1) { 
+        var startInLineSeg = (spritePosX & 7) << 2;
+        posInSpriteCanvas = posInSpriteCanvas + startInLineSeg;
+        var i = 0;
+        var posInSpriteColorLine = 0;
+        for (i = startInLineSeg; i < 32; i = i + 4) {
+          canvasForSprite.data[posInSpriteCanvas + 0] = spriteColorLine[posInSpriteColorLine+0];
+          canvasForSprite.data[posInSpriteCanvas + 1] = spriteColorLine[posInSpriteColorLine+1];
+          canvasForSprite.data[posInSpriteCanvas + 2] = spriteColorLine[posInSpriteColorLine+2];
+          canvasForSprite.data[posInSpriteCanvas + 3] = spriteColorLine[posInSpriteColorLine+3];
+
+          posInSpriteCanvas = posInSpriteCanvas + 4;
+          posInSpriteColorLine = posInSpriteColorLine + 4;
+        }
+
+      } else if (lineScenario == 2) {
+        var startInLineSeg = 0;
+        var endInLineSeg = (spritePosX & 7) << 2;
+        var i = 0;
+        var posInSpriteColorLine = (xDimension - (spritePosX & 7)) << 2;
+        for (i = 0; i < endInLineSeg; i = i + 4) {
+          canvasForSprite.data[posInSpriteCanvas + 0] = spriteColorLine[posInSpriteColorLine+0];
+          canvasForSprite.data[posInSpriteCanvas + 1] = spriteColorLine[posInSpriteColorLine+1];
+          canvasForSprite.data[posInSpriteCanvas + 2] = spriteColorLine[posInSpriteColorLine+2];
+          canvasForSprite.data[posInSpriteCanvas + 3] = spriteColorLine[posInSpriteColorLine+3];
+
+          posInSpriteCanvas = posInSpriteCanvas + 4;
+          posInSpriteColorLine = posInSpriteColorLine + 4;
+        }
+
+      } else {
+        var i = 0;
+        var posInSpriteColorLine = (lineSegmentStart - spritePosX) << 2;
+        for (i = 0; i < 32; i = i + 4) {
+          canvasForSprite.data[posInSpriteCanvas + 0] = spriteColorLine[posInSpriteColorLine+0];
+          canvasForSprite.data[posInSpriteCanvas + 1] = spriteColorLine[posInSpriteColorLine+1];
+          canvasForSprite.data[posInSpriteCanvas + 2] = spriteColorLine[posInSpriteColorLine+2];
+          canvasForSprite.data[posInSpriteCanvas + 3] = spriteColorLine[posInSpriteColorLine+3];
+
+          posInSpriteCanvas = posInSpriteCanvas + 4;
+          posInSpriteColorLine = posInSpriteColorLine + 4;
+        }
+
+
+      }
+
+    }
+  }
+
 
   function drawBitmapModeMultiColor(charPos) {
     var baseCharAdd = (registers[0x18] >> 1) & 7;    
@@ -249,15 +391,157 @@ function video(mycanvas, mem, cpu) {
 
   }
 
+ function populateSpriteColorLine (spriteNumber, spriteLine) {
+    var spriteData = getSpriteLineData(spriteNumber, spriteLine);
+    var spriteColor = registers[0x27 + spriteNumber] & 0xf;
+    i = 0;
+    posInColorLine = 0;
+    for (i = 0; i < 24; i++) {
+      var currentBit = (spriteData >> 23) & 1;
+      if (currentBit == 1) {
+        spriteColorLine [posInColorLine + 0] = colors[spriteColor][0];
+        spriteColorLine [posInColorLine + 1] = colors[spriteColor][1];
+        spriteColorLine [posInColorLine + 2] = colors[spriteColor][2];
+        spriteColorLine [posInColorLine + 3] = 255;
+      } else {
+        spriteColorLine [posInColorLine + 0] = 0;
+        spriteColorLine [posInColorLine + 1] = 0;
+        spriteColorLine [posInColorLine + 2] = 0;
+        spriteColorLine [posInColorLine + 3] = 0;
+      }
+      posInColorLine = posInColorLine + 4;
+      spriteData = (spriteData << 1) & 0xffffff;
+    }
+  }
+
+
+  function populateSpriteMultiColorLine (spriteNumber, spriteLine) {
+    var spriteData = getSpriteLineData(spriteNumber, spriteLine);
+
+    var spriteColor = registers[0x27 + spriteNumber] & 0xf;
+    var multicolorPalette = [-1, (registers[0x25] & 0xf), spriteColor, (registers[0x26] & 0xf)];
+    i = 0;
+    posInColorLine = 0;
+    for (i = 0; i < 12; i++) {
+      var currentBits = (spriteData >> 22) & 3;
+      if (currentBits > 0) {
+        spriteColorLine [posInColorLine + 0] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 1] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 2] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 3] = 255;
+        spriteColorLine [posInColorLine + 4] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 5] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 6] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 7] = 255;
+
+      } else {
+        spriteColorLine [posInColorLine + 0] = 0;
+        spriteColorLine [posInColorLine + 1] = 0;
+        spriteColorLine [posInColorLine + 2] = 0;
+        spriteColorLine [posInColorLine + 3] = 0;
+        spriteColorLine [posInColorLine + 4] = 0;
+        spriteColorLine [posInColorLine + 5] = 0;
+        spriteColorLine [posInColorLine + 6] = 0;
+        spriteColorLine [posInColorLine + 7] = 0;
+
+      }
+      posInColorLine = posInColorLine + 8;
+      spriteData = (spriteData << 2) & 0xffffff;
+    }
+  }
+
+  function populateSpriteColorLineExpanded (spriteNumber, spriteLine) {
+    var spriteData = getSpriteLineData(spriteNumber, spriteLine);
+    var spriteColor = registers[0x27 + spriteNumber] & 0xf;
+    i = 0;
+    posInColorLine = 0;
+    for (i = 0; i < 24; i++) {
+      var currentBit = (spriteData >> 23) & 1;
+      if (currentBit == 1) {
+        spriteColorLine [posInColorLine + 0] = colors[spriteColor][0];
+        spriteColorLine [posInColorLine + 1] = colors[spriteColor][1];
+        spriteColorLine [posInColorLine + 2] = colors[spriteColor][2];
+        spriteColorLine [posInColorLine + 3] = 255;
+        spriteColorLine [posInColorLine + 4] = colors[spriteColor][0];
+        spriteColorLine [posInColorLine + 5] = colors[spriteColor][1];
+        spriteColorLine [posInColorLine + 6] = colors[spriteColor][2];
+        spriteColorLine [posInColorLine + 7] = 255;
+
+      } else {
+        spriteColorLine [posInColorLine + 0] = 0;
+        spriteColorLine [posInColorLine + 1] = 0;
+        spriteColorLine [posInColorLine + 2] = 0;
+        spriteColorLine [posInColorLine + 3] = 0;
+        spriteColorLine [posInColorLine + 4] = 0;
+        spriteColorLine [posInColorLine + 5] = 0;
+        spriteColorLine [posInColorLine + 6] = 0;
+        spriteColorLine [posInColorLine + 7] = 0;
+      }
+      posInColorLine = posInColorLine + 8;
+      spriteData = (spriteData << 1) & 0xffffff;
+    }
+  }
+
+  function populateSpriteMultiColorLineExpanded (spriteNumber, spriteLine) {
+    var spriteData = getSpriteLineData(spriteNumber, spriteLine);
+
+    var spriteColor = registers[0x27 + spriteNumber] & 0xf;
+    var multicolorPalette = [-1, (registers[0x25] & 0xf), spriteColor, (registers[0x26] & 0xf)];
+    i = 0;
+    posInColorLine = 0;
+    for (i = 0; i < 12; i++) {
+      var currentBits = (spriteData >> 22) & 3;
+      if (currentBits > 0) {
+        spriteColorLine [posInColorLine + 0] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 1] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 2] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 3] = 255;
+        spriteColorLine [posInColorLine + 4] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 5] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 6] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 7] = 255;
+        spriteColorLine [posInColorLine + 8] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 9] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 10] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 11] = 255;
+        spriteColorLine [posInColorLine + 12] = colors[multicolorPalette[currentBits]][0];
+        spriteColorLine [posInColorLine + 13] = colors[multicolorPalette[currentBits]][1];
+        spriteColorLine [posInColorLine + 14] = colors[multicolorPalette[currentBits]][2];
+        spriteColorLine [posInColorLine + 15] = 255;
+
+      } else {
+        spriteColorLine [posInColorLine + 0] = 0;
+        spriteColorLine [posInColorLine + 1] = 0;
+        spriteColorLine [posInColorLine + 2] = 0;
+        spriteColorLine [posInColorLine + 3] = 0;
+        spriteColorLine [posInColorLine + 4] = 0;
+        spriteColorLine [posInColorLine + 5] = 0;
+        spriteColorLine [posInColorLine + 6] = 0;
+        spriteColorLine [posInColorLine + 7] = 0;
+        spriteColorLine [posInColorLine + 8] = 0;
+        spriteColorLine [posInColorLine + 9] = 0;
+        spriteColorLine [posInColorLine + 10] = 0;
+        spriteColorLine [posInColorLine + 11] = 0;
+        spriteColorLine [posInColorLine + 12] = 0;
+        spriteColorLine [posInColorLine + 13] = 0;
+        spriteColorLine [posInColorLine + 14] = 0;
+        spriteColorLine [posInColorLine + 15] = 0;
+
+      }
+      posInColorLine = posInColorLine + 16;
+      spriteData = (spriteData << 2) & 0xffffff;
+    }
+  }
+
 
   function fillBorderColor() {
     var borderColor = registers[0x20] & 0xf;
     var i;
     for (i = 0; i < 8; i++ ) {
-      imgData.data[posInCanvas + 0] = colors[borderColor][0];
-      imgData.data[posInCanvas + 1] = colors[borderColor][1];
-      imgData.data[posInCanvas + 2] = colors[borderColor][2];
-      imgData.data[posInCanvas + 3] = 255;
+      foregroundData.data[posInCanvas + 0] = colors[borderColor][0];
+      foregroundData.data[posInCanvas + 1] = colors[borderColor][1];
+      foregroundData.data[posInCanvas + 2] = colors[borderColor][2];
+      foregroundData.data[posInCanvas + 3] = 255;
       posInCanvas = posInCanvas + 4;
     }
 
