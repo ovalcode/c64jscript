c3f246be1dbfb51662c983f7514a14b3525e8e87
johan <johan@johan>
2019-03-24 17:04:12 +0200

    modified:   Disk.js

--- Disk.js
+++ Disk.js
@@ -1,6 +1,6 @@
 function disk(alarmManager) {
   var myAlarmManager = alarmManager;
-  var isEnabled = true;
+  var isEnabled = false;
   var ticksBeforeExpiry = 500;
   var atnbit = 0;
   var c64databit = 0;
@@ -12,7 +12,10 @@ function disk(alarmManager) {
   var bitCounter = 0;
   var dataByte = 0;
   var prevClk = 0;
+  var initialised = false;
   var receiveFileName = false;
+  var unlisten = false;
+  var waitForAtnClear = false;
 
   myAlarmManager.addAlarm(this);
  
@@ -30,32 +33,74 @@ function disk(alarmManager) {
       //dataByte = 0;
       //bitCounter = 8;
       receiveFileName = true;                
+    } else if (commandByte == 0x3f) {
+      unlisten = true;
+      waitForAtnClear = true;
+      sheduleTimeout(1000);
     }
   }
 
   this.write = function(byteval) {
     atnbit = (byteval & 8) ? 1 : 0;
+    if (atnbit == 0)
+      waitForAtnClear = false;
     c64databit = (byteval & 32) ? 1 : 0;
     clkbit = (byteval & 16) ? 1 : 0;
+    if (!initialised) {
+      if (clkbit == 1) {
+        initialised = 1;
+        state = 1;
+      }
+      return;
+    }
+    if (!unlisten)
+      isEnabled = false;
+   // if (unlisten/* & !waitForAtnClear*/) {
+   //   if (atnbit == 1) 
+   //     unlisten = false;
+   // }
+    if (unlisten)
+      return;
+    processListenState (false);
     //console.log(byteval.toString(2));
-    if (state == 2) {
+
+  }
+
+  function processListenState(timeOut) {
+    if (unlisten)
+      unlisten = false;
+    if (state == 1) {
+      if (!clkbit) {
+        started = true;
+        localData = 1;
+        state = 2;
+        //isEnabled = false;
+        bitCounter = 8;
+        dataByte = 0;
+        sheduleTimeout(250);
+      }
+    } else if (state == 2) {
       if (clkbit) {
         state = 3; //receive data on clock high
         prevClk = clkbit;
+      } else if (timeOut) {
+        localData = 0;
+        state = 1;
+        sheduleTimeout(80);        
       }
-      sheduleTimeout(250);
+      //sheduleTimeout(250);
     } else if (state == 3) {
       if ((prevClk != clkbit) & !clkbit) {
-        console.log("Data bit captured: "+ c64databit);
+        //console.log("Data bit captured: "+ c64databit);
         dataByte = (dataByte >> 1) | (!c64databit ? 0x80 : 0);
         bitCounter--;
         sheduleTimeout(250);
         if (!bitCounter) {          
           console.log("Byte received: " + dataByte+ " atn: " + atnbit);
           //if (!receiveFileName) {
-            isEnabled = true;
+            //isEnabled = true;
             state = 1;
-            ticksBeforeExpiry = 500;
+            //ticksBeforeExpiry = 500;
             localData = 0;
           //} else {
           //  state = 2;
@@ -86,10 +131,13 @@ function disk(alarmManager) {
 
   function sheduleTimeout(cycles) {
     ticksBeforeExpiry = cycles;
+    isEnabled = true;
   }
 
   this.trigger = function() {
-    if (state == 0) {
+    isEnabled = false;  
+    processListenState(true);
+  /*if (state == 0) {
       if (clkbit == 1)
         state = 1;
       ticksBeforeExpiry = 500;
@@ -126,8 +174,8 @@ function disk(alarmManager) {
       //localData = 1;
     //}
     //ticksBeforeExpiry = 500;
-  }
-
+  }*/
+  }  
 }
 
 
